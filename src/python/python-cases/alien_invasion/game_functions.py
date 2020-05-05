62253f6854888ade275854c7433cc17608e4b0d1
Lang <silentbalanceyh@126.com>
2020-05-05 13:42:32 +0800

    外星人基础

--- src/python/python-cases/alien_invasion/game_functions.py
+++ src/python/python-cases/alien_invasion/game_functions.py
@@ -1,13 +1,26 @@
 import sys
 
 import pygame
+from bullet import Bullet
 
-def check_keydown_events(event, ship):
+
+def check_keydown_events(event, ai_settings, screen, ship ,bullets):
     if event.key == pygame.K_RIGHT:
         # 向右移动飞船
         ship.moving_right = True
     elif event.key == pygame.K_LEFT:
         ship.moving_left = True
+    elif event.key == pygame.K_SPACE:
+        fire_bullet(ai_settings, screen, ship, bullets)
+
+
+def fire_bullet(ai_settings, screen, ship, bullets):
+    """如果还没达到限制，就发射一颗子弹"""
+    # 创建新子弹，并且加入到编组 bullets 中
+    if len(bullets) < ai_settings.bullets_allowed:
+        new_bullet = Bullet(ai_settings, screen, ship)
+        bullets.add(new_bullet)
+
 
 def check_keyup_events(event, ship):
     if event.key == pygame.K_RIGHT:
@@ -15,22 +28,35 @@ def check_keyup_events(event, ship):
     elif event.key == pygame.K_LEFT:
         ship.moving_left = False
 
-def check_events(ship):
+
+def check_events(ai_settings, screen, ship, bullets):
     """响应键盘和鼠标事件"""
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
         elif event.type == pygame.KEYDOWN:
-            check_keydown_events(event, ship)
+            check_keydown_events(event, ai_settings, screen, ship, bullets)
         elif event.type == pygame.KEYUP:
             check_keyup_events(event, ship)
 
 
-def update_screen(ai_settings, screen, ship):
+def update_screen(ai_settings, screen, ship, bullets):
     """更新屏幕上的图像，并切换到新屏幕"""
     # 每次循环重回屏幕
     screen.fill(ai_settings.bg_color)
+    # 在飞船和外星人后面重绘所有子弹
+    for bullet in bullets:
+        bullet.draw_bullet()
     ship.blitme()
 
     # 让最近绘制的屏幕可见
     pygame.display.flip()
+
+def update_bullets(bullets):
+    """更新子弹的位置，并且删除已经消失的子弹"""
+    bullets.update()
+
+    # 删除已消失的子弹
+    for bullet in bullets.copy():
+        if bullet.rect.bottom <= 0:
+            bullets.remove(bullet)
